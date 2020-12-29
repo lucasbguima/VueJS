d3fd40d19e138476faaf0fd9386cfa4291510ba8
lucasbguima <lucasbrandaoguimaraes@gmail.com>
2020-12-29 00:47:43 -0200

    Aula 05 - Completa

---
+++ Aula 5/aula5/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/base',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
